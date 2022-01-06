00a1445d03f0f7c5f6672ec358b2e72a2b23c1f0
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-01-06 10:46:25 +0530

    :art: changes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,8 @@
-import Head from 'next/head';
-import { RecoilRoot } from 'recoil';
-import '../styles/globals.scss';
-import { ThemeProvider } from 'next-themes';
-import { SafeHydrate } from '@commons/helpers/ssr-utils';
+import Head from "next/head";
+import { RecoilRoot } from "recoil";
+import "../styles/globals.scss";
+import { ThemeProvider } from "next-themes";
+import { SafeHydrate } from "@commons/helpers/ssr-utils";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
@@ -10,6 +10,12 @@ const MyApp = ({ Component, pageProps }) => {
       <RecoilRoot>
         <Head>
           <title>Typing Guru</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Redressed&family=Rhodium+Libre&family=Risque&family=Ropa+Sans&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         {!Component.SSR && (
           <SafeHydrate>

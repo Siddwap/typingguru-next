fedb4095ca751b5fb027daea83b95f7c7ef8f9b2
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-01-08 19:23:08 +0530

    :art: changes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,40 +1,33 @@
-import Head from 'next/head';
 import { RecoilRoot } from 'recoil';
 import '../styles/globals.scss';
 import { ThemeProvider } from 'next-themes';
 import { SafeHydrate } from '@commons/helpers/ssr-utils';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  const handleRouteChange = (url) => {
+    window.gtag('config', '[Tracking ID]', {
+      page_path: url,
+    });
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider attribute="class">
+      <Head>
+        <title>Typing Guru</title>
+      </Head>
       <RecoilRoot>
-        <Head>
-          <title>Typing Guru</title>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Redressed&family=Rhodium+Libre&family=Risque&family=Roboto+Mono&family=Ropa+Sans&display=swap"
-            rel="stylesheet"
-          />
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-J7DGFTDF5Q"
-          />
-
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-J7DGFTDF5Q');
-              gtag('config', '[Tracking ID]', { page_path: window.location.pathname });
-            `,
-            }}
-          />
-        </Head>
         {!Component.SSR && (
           <SafeHydrate>
             <Component {...pageProps} />

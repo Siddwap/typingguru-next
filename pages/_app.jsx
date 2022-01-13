cf497d1dd80fb8c01686084c071d282a8f8e6221
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-01-13 23:13:16 +0530

    :art: bugfix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -34,37 +34,6 @@ const MyApp = ({ Component, pageProps }) => {
           content="A complete platform to learn typings and to practice your typing skills. Learn to type faster, get better at typing guru, and improve your typing skills."
         />
 
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Redressed&family=Rhodium+Libre&family=Risque&family=Roboto+Mono&family=Ropa+Sans&display=swap"
-            rel="stylesheet"
-          />
-
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-QW6470DZ7Z"
-          />
-
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-QW6470DZ7Z', { page_path: window.location.pathname });
-              `,
-            }}
-          />
-        </Head>
-
         <link rel="icon" href="/DialogIcon.png" />
         <link rel="apple-touch-icon" href="/DialogIcon.png" />
         <link rel="manifest" href="/manifest.json" />

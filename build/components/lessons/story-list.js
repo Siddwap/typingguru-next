9cb4c661ef85af5d2b21d6cae6abc07a58b6bfe4
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-09-10 11:09:26 +0530

    :bug: story bug fixed

--- build/components/lessons/story-list.js
+++ build/components/lessons/story-list.js
@@ -5,7 +5,12 @@ const StoryList = [
   lsns.STORY2,
   lsns.STORY3,
   lsns.STORY4,
-  lsns.STORY5,
+  lsns.STORY5.replace(/[\t\n]/g, ' '),
+  lsns.STORY6.replace(/[\t\n]/g, ' '),
+  lsns.STORY7.replace(/[\t\n]/g, ' '),
+  // lsns.STORY8,
+  // lsns.STORY9,
+  // lsns.STORY10,
 ];
 
 export default StoryList;
